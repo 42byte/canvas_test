408dd16891a6b2bb6b9792f429149d6c9305ff67
42byte <18312138+42byte@users.noreply.github.com>
2020-10-20 17:24:38 +0200

    Added harmoniser function, not working yet.

--- app.js
+++ app.js
@@ -2,56 +2,133 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 
-let count_rows = canvas.height / 10;
-let count_columns = canvas.width / 10;
+let count_rows = canvas.height / 60;
+let count_columns = canvas.width / 60;
 console.log("rows: " + String(count_rows) + ", cols: " + String(count_columns));
 
+//constructor function cell
+function Cell(content, x, y, color) {
+  this.content = content;
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.draw = function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x * 40, this.y * 40, 38, 38);
+    ctx.fillStyle = "black";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "hanging";
+    ctx.fillText(content, this.x * 40, this.y *40);
+  };
+}
 
 //creating the grid
-function createGrid() {
+function createGrid(populate=1, harmonize=0) {
     let grid = [];
     for(let i = 0; i < count_rows; i++) {
         var temp = [];
         for(let j = 0; j < count_columns; j++){
-            var content = "|" + String(i) + ", " + String(j) + "|";
+          //decide the fertility
+          //randomly place plants
+          //color cells accordingly
+          if(populate){
+            var content = new Cell(randomize(10), j, i, "orange");
+            content.draw();
             temp.push(content);
-            ctx.fillStyle = "orange";
-            ctx.fillRect(j * 40, i *40, 38, 38);
-            ctx.fillStyle = "black";
-            ctx.textAlign = "left";
-            ctx.textBaseline = "hanging";
-            ctx.fillText(content, j * 40, i *40);
+          } else {
+            temp.push(0);
+          }
         }
         grid.push(temp);
     }
+
+    console.log("grid complete");
+
+    if(harmonize){
+      //harmonise with neighbours
+      console.log("HARMONISE!");
+      harmonise(grid, 1);
+    }
+
     return grid;
 }
 
-//createGrid();
+
+function harmonise(grid, acceptable_range) {
+  //Go through grid and check if every value is within an acceptable range of
+  //average of their neighbours
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[0].length; j++){
+      //Get the values of neighbours
+      var neighbour = neighborCells(grid, i, j, 1, true);
+
+      //Calculate average
+      var avg = getAverage(neighbour);
+      console.log("Average: " + String(avg));
+
+
+      //If the value of the position is within the acceptable range of the average,
+      //nothing is done. If not, the value is changed to be within range.
+      if(grid[i][j].content > Math.abs(avg) /*+ acceptable_range*/) {
+        //console.log(grid[i][j]).content)
+        console.log("Harmonised! Old value: " + String(grid[i][j].content));
+        grid[i][j].content = Math.floor(Math.abs(avg) + (Math.random() -0.5) * acceptable_range * 2);
+        grid[i][j].draw();
+        console.log("New Value: " + String(grid[i][j].content));
+      }
+    }
+  }
+}
+
+function getAverage(array){
+  var total = 0;
+  for(var x = 0; x < array.length; x++) {
+    total += array[x];
+  }
+  return total / array.length;
+}
+
 
 //Randomizer
-
+function randomize(num) {
+  return Math.floor(Math.random() * num)
+}
 
 //Check around plant position
 
 
-function neighborCells(matrix, row, column, sight) {
+function neighborCells(matrix, row, column, sight, returnArray) {
+    let retArr = [];
     row_limit = matrix.length;
     if(row_limit > 0){
     column_limit = matrix[0].length;
     for(x = Math.max(0, row-sight); x <= Math.min(row+sight, row_limit); x++){
         for(y = Math.max(0, column-sight); y <= Math.min(column+sight, column_limit); y++){
         if(x != row || y != column){
-            console.log(matrix[x][y]);
+            if(returnArray){
+              //console.log(matrix[x][y]);
+              retArr.push(matrix[x][y].content);
+            } else {
+              //console.log(matrix[x][y]);
+              //return matrix[x][y];
+            }
         }
         }
     }
+    return retArr
     }
 }
 
-var new_grid = createGrid();
-console.log("position: " + String(new_grid[2][2]));
-neighborCells(new_grid, 2, 2, 1);
+
+function createGame() {
+  console.log("start");
+  const base_layer = createGrid(1, 1);
+
+}
+createGame();
+//console.log("position: " + String(base_layer[2][2]));
+//neighborCells(new_grid, 2, 2, 1);
+
 
 //inject --> pick random position, check if position is okay, plant
 
@@ -85,8 +162,3 @@ function startUpdate() {
 function stopUpdate() {
     window.clearTimeout(updateFrame);
 }
-
-
-
-
-
